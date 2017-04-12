8b6d4848a084ebba4622778aff56628a3dbf9462
arith-kawai <kawai@arith-metic.jp>
2017-04-13 03:01:40 +0900

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,12 @@
      output: {
          path: process.env.PWD + '/bin',
          filename: 'app.bundle.js'
+     },
+     module: {
+         loaders: [{
+             test: /\.js$/,
+             exclude: /node_modules/,
+             loader: 'babel-loader'
+         }]
      }
  };
