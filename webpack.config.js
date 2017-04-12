709be9a74cd506229eec753cb61c3dbf370f1efc
arith-kawai <kawai@arith-metic.jp>
2017-04-13 04:29:28 +0900

    add html loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,33 @@
- module.exports = {
-     entry: process.env.PWD + '/src/app.js',
+var path = require("path");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+     entry: path.resolve(__dirname, "src/js/app.js"),
      output: {
-         path: process.env.PWD + '/bin',
-         filename: 'app.bundle.js'
+	path: path.resolve(__dirname, "public/js"),
+	//publicPath: "http://localhost:8889/assets/",
+         filename: 'app.js'
      },
      module: {
-         loaders: [{
-             test: /\.js$/,
-             exclude: /node_modules/,
-             loader: 'babel-loader'
-         }]
-     }
+         loaders: [
+		{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loader: 'babel-loader'
+		}
+	]
+     },
+  plugins: [
+    new HtmlWebpackPlugin({  // Also generate a test.html 
+      filename: path.resolve(__dirname, 'public/index.html'),
+      template: 'src/index.html'
+    })
+  ],
+devtool: 'source-map',
+	devServer: {
+		contentBase: path.resolve(__dirname, 'public'),
+		inline: true,
+		hot: true,
+		port: 8889
+	}
  };
